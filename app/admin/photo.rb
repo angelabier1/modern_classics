b5df9624000a409701fb75f9943b66a34150dfef
Prelang Builder <builder@prelang.com>
2015-09-25 15:11:39 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Photo' to allow for create, update

--- app/admin/photo.rb
+++ app/admin/photo.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Photo do
+  permit_params :image_file_name, :image_content_type, :image_file_size, :direct_upload_url, :processed, :primary
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
