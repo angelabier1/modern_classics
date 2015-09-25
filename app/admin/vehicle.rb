c13df00114143860d1be06b84b8e4ed8ca095f31
Prelang Builder <builder@prelang.com>
2015-09-25 15:11:52 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Vehicle' to allow for create, update

--- app/admin/vehicle.rb
+++ app/admin/vehicle.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Vehicle do
+  permit_params :vin, :mileage, :year, :make, :model, :transmission, :cylinders, :drivetrain, :body_type, :fuel_type, :body_style, :title_in, :mileage_is, :new_used, :exterior_color, :interior_color, :trim, :doors, :inventory_source, :engine, :highway_mpg, :wheels, :condition, :tag_number, :photos_taken, :active, :options, :city_mpg, :nada_wholesale, :nada_retail, :sale_status, :est_recond_cost, :age, :comments
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
